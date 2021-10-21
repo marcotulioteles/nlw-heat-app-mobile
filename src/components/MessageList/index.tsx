d5d764aaa267714ec5624718e7b854c8b50f328a
Marco Tulio Teles <marcotuliocivileng@gmail.com>
2021-10-21 19:22:14 -0300

    created Message and Message List Components

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+import { Message } from '../Message';
+
+import { styles } from './styles';
+
+const message = {
+  id: '1',
+  text: 'message test',
+  user: {
+    name: "Marco Túlio Teles",
+    avatar_url: 'https://github.com/marcotulioteles.png'
+  }
+}
+
+export function MessageList(){
+  return (
+    <ScrollView 
+      style={styles.container}
+      contentContainerStyle={styles.content}
+      keyboardShouldPersistTaps="never"
+    >
+      <Message 
+        data={message}
+      />
+    </ScrollView>
+  );
+}
