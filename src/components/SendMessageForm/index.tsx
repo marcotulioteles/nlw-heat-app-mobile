e343cbda1275110eb88827043bb6160d51e5a3f2
Marco Tulio Teles <marcotuliocivileng@gmail.com>
2021-10-22 15:30:41 -0300

    Sending Messages w/ useEffect on SendMessageForm

--- src/components/SendMessageForm/index.tsx
+++ src/components/SendMessageForm/index.tsx
@@ -1,18 +1,34 @@
 import React, { useState } from 'react';
 
-import { View, TextInput} from 'react-native';
+import { View, TextInput, Alert, Keyboard } from 'react-native';
+import { api } from '../../services/api';
 import { COLORS } from '../../theme';
 import { Button } from '../Button';
 
 import { styles } from './styles';
 
-export function SendMessageForm(){
+export function SendMessageForm() {
   const [message, setMessage] = useState('');
   const [sendingMessage, setSendingMessage] = useState(false);
 
+  async function handleMessagesSubmit() {
+    const messageFormatted = message.trim();
+
+    if (messageFormatted.length > 0) {
+      setSendingMessage(true);
+      await api.post('/messages', { message: messageFormatted });
+      setMessage('');
+      Keyboard.dismiss();
+      setSendingMessage(false);
+      Alert.alert('Mensagem enviada com sucesso!');
+    } else {
+      Alert.alert('Escreva a mensagem para enviar')
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <TextInput 
+      <TextInput
         keyboardAppearance="dark"
         placeholder="Qual a sua expectativa para o evento ?"
         placeholderTextColor={COLORS.GRAY_PRIMARY}
@@ -23,10 +39,12 @@ export function SendMessageForm(){
         style={styles.input}
         editable={!sendingMessage}
       />
-      <Button 
+      <Button
         title="ENVIAR MENSAGEM"
         backgroundColor={COLORS.PINK}
         color={COLORS.WHITE}
+        isLoading={sendingMessage}
+        onPress={handleMessagesSubmit}
       />
     </View>
   );
